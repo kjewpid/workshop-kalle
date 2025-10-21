f41e6a44629879e1bef8abbdd2a54c92f3d52f0f
Kalle Hallbäck <kalle.hallback@gmail.com>
2025-10-21 14:28:49 +0200

    kalle ändrar println

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main { 
     public static void main(String[] args) { 
-          System.out.println("Hello from Person B!");   // Ändring av Person B 
+          System.out.println("Hello fromm Person B!");   // Ändring av Person B 
     } 
 }
 
